7a591ac402686db28ea585dcd96946f8612e781b
Graeme Henderson <graeme.189@gmail.com>
2019-08-07 18:28:03 +0100

    Updated CountriesList and ListComponent files

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,31 @@
   <div class="">
     <h1>Countries</h1>
     <div class="container">
-      <li v-for="country in countries">{{ country.name }}</li>
+      <countries-list :countries='countries'></countries-list>
     </div>
   </div>
 </template>
 
 <script>
+import CountriesList from './components/CountriesList.vue'
+import CountryDetail from './components/CountryDetail.vue'
+
 export default {
   name: 'app',
   data(){
     return {
       countries: []
-    }
+    };
   },
   mounted(){
     const response = fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
     // console.log(response); //check this returns to the console
     .then(countries => this.countries = countries)
+  },
+  components: {
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
